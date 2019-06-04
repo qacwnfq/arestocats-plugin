496e86d5637df002a1ca229c61b3ccff8bcc7721
Fredrik Jadebeck <fredrik.jadebeck@aixigo.de>
2019-06-04 14:04:01 +0200

    test snyk

--- src/main/java/org/jenkinsci/plugins/arestocats/ArestocatsProjectSummaryAction.java
+++
@@ -1,4 +0,0 @@
-package org.jenkinsci.plugins.arestocats;
-
-public class ArestocatsProjectSummaryAction {
-}
--- src/main/java/org/jenkinsci/plugins/arestocats/Paths.java
+++
@@ -1,7 +0,0 @@
-package org.jenkinsci.plugins.arestocats;
-
-public interface Paths {
-    public static final String BASE = "arestocats";
-    public static final String METRICS = "metrics";
-    public static final String RESULTS = "results";
-}
